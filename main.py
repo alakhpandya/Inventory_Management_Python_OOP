d310f06d02419a397127ce855e1aa0d7486a29ac
alakhpandya <alakhpandya@gmail.com>
2022-11-20 14:52:33 +0530

    20 nov commit

--- main.py
+++ main.py
@@ -29,13 +29,42 @@ a1 = Accessories('Sandisk', 'Pen Drive', 20, 1200, 1050, 2)
 # Products.printAllStock() 
 # print(l1.__class__.__name__)
 
+while True:
+    print("Press 1 to make a sell")
+    print("Press 2 to make a purchase")
+    print("Press 3 to see the inventory")
+    print("Press 4 to add new product")
+    print("Press 5 to remove an existing product")
+    print("Press 6 to edit a product")
 
-print("Press 1 to make a sell")
-print("Press 2 to make a purchase")
-choice = int(input())
+    print("Press 8 to exit without saving")
+    print("Press 9 to save & exit")
 
-if choice == 1:
-    Products.generateBill()
+    choice = int(input())
 
-elif choice == 2:
-    pass
+    if choice == 1:
+        Products.generateBill()
+
+    elif choice == 2:
+        Products.makePurchase()
+
+    elif choice == 3:
+        Products.printAllStock()
+
+    elif choice == 4:
+        pass
+
+    elif choice == 5:
+        pass
+    
+    elif choice == 6:
+        pass
+    
+    elif choice == 7:
+        pass
+    
+    elif choice == 8:
+        break
+
+    elif choice == 9:
+        pass
--- products.py
+++ products.py
@@ -1,9 +1,10 @@
 import csv
+from datetime import datetime
 
 class Products():
-
+    ideal_stock = 10
     all_products = []
-    all_bills = []
+    all_bills = [] 
     def __init__(self, brand, model, units, mrp, dealerPrice = 0):
         assert brand.__class__.__name__ == 'str', f'Please enter string only'
         assert model.__class__.__name__ == 'str', f'Please enter string only'
@@ -60,6 +61,9 @@ class Products():
 
     @staticmethod
     def generateBill():
+        # date = datetime.today()
+        day = input("Enter Bill Date (dd/mm/yyyy): ")
+        date = datetime.strptime(day, "%d/%m/%Y")
         current_bill = []
         print("Enter the quantity sold:")
         for product in Products.all_products:
@@ -67,8 +71,8 @@ class Products():
             billing_price = product.sold * product.mrp
             profit = product.sold * product.profit
             current_bill.append((product.brand, product.model, product.sold, product.mrp, billing_price, profit))
-        # print(current_bill)
-        Products.all_bills.append(current_bill)
+            product.units -= product.sold
+        
         print()
         print("Your Bill".center(70, "-"))
         print("Sr.No\t" + "Name\t".expandtabs(30) + "Qty\t" + "Rate\tAmount".expandtabs(10))
@@ -82,8 +86,40 @@ class Products():
 
         print("-"*70)
         print(f"{total}".rjust(65))
+        current_bill.append(date)
+        print(current_bill)
+        Products.all_bills.append(current_bill)
+
+    @staticmethod
+    def makePurchase():
+        print("Sr.No\t" + "Name\t".expandtabs(30) + "Price\tStock\tIdeal\tRecomd\tOrder")
+        i = 1
+        total = 0
+        for product in Products.all_products:
+            recommended = 0 if product.units >= product.ideal_stock else product.ideal_stock - product.units
+            order = int(input(f"{i}\t" + f"{product.brand} - {product.model}\t".expandtabs(30) + f"{product.dealerPrice}\t{product.units}\t{product.ideal_stock}\t{recommended}\t"))
+            print("Amount =", product.dealerPrice*order)
+            total += product.dealerPrice*order
+            
+            product.units += order
+            i += 1
+
+        print("Total =", total)
+
+    @staticmethod
+    def showProfit():
+        start = input("From (dd/mm/yyyy): ")
+        stop = input("To (dd/mm/yyyy): ")
+        begin = datetime.strptime(start, "%d/%m/%Y")
+        end = datetime.strptime(stop, "%d/%m/%Y")
+        total_profit = 0
+        for bill in Products.all_bills:
+            if begin <= bill[-1] <=end:
+                total_profit += bill[-2]
+        print("Total Profit =", total_profit)
 
 if __name__ == "__main__":
     Products.import_from_csv()
     # print(Products.all_products)
     Products.generateBill()
+    # Products.makePurchase()
